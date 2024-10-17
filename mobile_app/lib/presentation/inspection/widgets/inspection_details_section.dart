023cb247130f8dc59dde5c31bcaf078e3b77e2c0
SzyMan1337 <szymanski.mateusz133@gmail.com>
2024-10-17 19:47:49 +0200

    Mobile: Add explanatory messages for tire condition based on defect probability

--- mobile_app/lib/presentation/inspection/widgets/inspection_details_section.dart
+++ mobile_app/lib/presentation/inspection/widgets/inspection_details_section.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tireinspectorai_app/common/common.dart';
 import 'package:tireinspectorai_app/domain/domain.dart';
+import 'package:tireinspectorai_app/l10n/localization_provider.dart';
 
-class InspectionDetailsSection extends StatelessWidget {
+class InspectionDetailsSection extends ConsumerStatefulWidget {
   final double probabilityScore;
   final InspectionModel modelUsed;
   final bool isDefective;
@@ -16,9 +18,17 @@ class InspectionDetailsSection extends StatelessWidget {
   });
 
   @override
+  ConsumerState<InspectionDetailsSection> createState() =>
+      _InspectionDetailsSectionState();
+}
+
+class _InspectionDetailsSectionState
+    extends ConsumerState<InspectionDetailsSection> {
+  @override
   Widget build(BuildContext context) {
-    final l10n = AppLocalizations.of(context)!;
-    final statusText = isDefective ? l10n.defectiveStatus : l10n.goodStatus;
+    final l10n = ref.watch(localizationProvider);
+    final statusText =
+        widget.isDefective ? l10n.defectiveStatus : l10n.goodStatus;
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -34,9 +44,41 @@ class InspectionDetailsSection extends StatelessWidget {
               TextSpan(
                 text: statusText,
                 style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                      color: isDefective ? Colors.red : Colors.green,
+                      color: widget.isDefective ? Colors.red : Colors.green,
                       fontWeight: FontWeight.bold,
                     ),
+              ),
+              const WidgetSpan(child: GapWidgets.w8),
+              WidgetSpan(
+                alignment: PlaceholderAlignment.middle,
+                child: GestureDetector(
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text(l10n.statusExplanationTitle),
+                          content: Text(widget.isDefective
+                              ? l10n.defectiveStatusExplanation
+                              : l10n.goodStatusExplanation),
+                          actions: [
+                            TextButton(
+                              child: Text(l10n.closeButtonLabel),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: Icon(
+                    Icons.info_outline,
+                    size: 18,
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+                ),
               ),
             ],
           ),
@@ -50,7 +92,7 @@ class InspectionDetailsSection extends StatelessWidget {
                 ),
             children: [
               TextSpan(
-                text: Helpers.getModelName(modelUsed, l10n),
+                text: Helpers.getModelName(widget.modelUsed, l10n),
                 style: Theme.of(context).textTheme.bodyLarge?.copyWith(
                       fontWeight: FontWeight.normal,
                     ),
@@ -67,7 +109,7 @@ class InspectionDetailsSection extends StatelessWidget {
                 ),
             children: [
               TextSpan(
-                text: '${(probabilityScore * 100).toStringAsFixed(2)}%',
+                text: '${(widget.probabilityScore * 100).toStringAsFixed(2)}%',
                 style: Theme.of(context).textTheme.bodyLarge?.copyWith(
                       fontWeight: FontWeight.normal,
                     ),
@@ -75,7 +117,35 @@ class InspectionDetailsSection extends StatelessWidget {
             ],
           ),
         ),
+        GapWidgets.h4,
+        _buildProbabilityScoreDescription(
+            context, widget.probabilityScore, l10n),
       ],
+    );
+  }
+
+  Widget _buildProbabilityScoreDescription(
+      BuildContext context, double probabilityScore, AppLocalizations l10n) {
+    String description;
+
+    if (probabilityScore <= 0.2) {
+      description = l10n.probabilityScoreDescriptionLow;
+    } else if (probabilityScore <= 0.4) {
+      description = l10n.probabilityScoreDescriptionModerateLow;
+    } else if (probabilityScore <= 0.6) {
+      description = l10n.probabilityScoreDescriptionModerate;
+    } else if (probabilityScore <= 0.8) {
+      description = l10n.probabilityScoreDescriptionHigh;
+    } else {
+      description = l10n.probabilityScoreDescriptionVeryHigh;
+    }
+
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Text(
+        description,
+        style: Theme.of(context).textTheme.bodyMedium,
+      ),
     );
   }
 }
