af963e9cad9b0623b4c076a33a455dacdc053617
SzyMan1337 <szymanski.mateusz133@gmail.com>
2024-10-20 17:01:17 +0200

    Cloud: Added APK download link for Android and App Store link for iOS

--- cloud_run_model_service/app/support.py
+++ cloud_run_model_service/app/support.py
@@ -1,12 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 support_bp = Blueprint("support_bp", __name__)
+
+
+@support_bp.route("/")
+def home():
+    return redirect(url_for("support_bp.support"))
 
 
 # Support page route
 @support_bp.route("/support")
 def support():
-    return render_template("support.html")
+    firebase_apk_link = "https://firebasestorage.googleapis.com/v0/b/tireinspectorai.appspot.com/o/android%2Fapp-release.apk?alt=media"
+    return render_template("support.html", firebase_apk_link=firebase_apk_link)
 
 
 # Privacy Policy page route
