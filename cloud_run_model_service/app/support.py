30d1ba695256b5a0219d1097ea689923aa23b4b3
SzyMan1337 <szymanski.mateusz133@gmail.com>
2024-10-20 12:58:05 +0200

    Cloud: Add privacy policy

--- cloud_run_model_service/app/support.py
+++ cloud_run_model_service/app/support.py
@@ -1,8 +1,15 @@
 from flask import Blueprint, render_template
 
-support_bp = Blueprint('support_bp', __name__)
+support_bp = Blueprint("support_bp", __name__)
+
 
 # Support page route
-@support_bp.route('/support')
+@support_bp.route("/support")
 def support():
-    return render_template('support.html')
+    return render_template("support.html")
+
+
+# Privacy Policy page route
+@support_bp.route("/privacy-policy")
+def privacy_policy():
+    return render_template("privacy_policy.html")
