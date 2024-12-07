74e46808ebb4b4cfcddda38f1718472ea84e43d5
SzyMan1337 <szymanski.mateusz133@gmail.com>
2024-12-07 08:44:43 +0100

    Mobile: Support for optional normalization

--- mobile_app/lib/data/utils/image_processing_helper.dart
+++ mobile_app/lib/data/utils/image_processing_helper.dart
@@ -2,8 +2,11 @@ import 'dart:io';
 import 'package:image/image.dart' as img;
 
 class ImageProcessingHelper {
-  // Method to process an image into a list of normalized float values
-  static Future<List<double>> processImage(String imageUrl) async {
+  // Method to process an image into a list of float values
+  static Future<List<double>> processImage(
+    String imageUrl, {
+    bool normalize = false,
+  }) async {
     File imageFile = File(imageUrl);
     img.Image? image = img.decodeImage(imageFile.readAsBytesSync());
 
@@ -14,12 +17,15 @@ class ImageProcessingHelper {
     // Resize the image to 224x224
     img.Image resizedImage = img.copyResize(image, width: 224, height: 224);
 
-    // Convert the resized image to a list of normalized values
-    return _imageToFloatList(resizedImage);
+    // Convert the resized image to a list of float values
+    return _imageToFloatList(resizedImage, normalize: normalize);
   }
 
-  // Private method to convert an image to a list of normalized float values
-  static List<double> _imageToFloatList(img.Image image) {
+  // Private method to convert an image to a list of float values
+  static List<double> _imageToFloatList(
+    img.Image image, {
+    bool normalize = false,
+  }) {
     List<double> input = [];
 
     for (int y = 0; y < image.height; y++) {
@@ -30,10 +36,17 @@ class ImageProcessingHelper {
         int g = pixel.g.toInt();
         int b = pixel.b.toInt();
 
-        // Normalize RGB values to the range [0, 1]
-        input.add(r / 255.0);
-        input.add(g / 255.0);
-        input.add(b / 255.0);
+        if (normalize) {
+          // Normalize RGB values to the range [0, 1]
+          input.add(r / 255.0);
+          input.add(g / 255.0);
+          input.add(b / 255.0);
+        } else {
+          // Add raw RGB values without normalization
+          input.add(r.toDouble());
+          input.add(g.toDouble());
+          input.add(b.toDouble());
+        }
       }
     }
 
